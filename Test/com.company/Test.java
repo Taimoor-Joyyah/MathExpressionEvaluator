67980ad8bd1b459d9c8838f9b1275ea7737c60c3
taimoor <mtaimoor0167@gmail.com>
2022-09-02 15:05:25 +0500

    Added Brackets fixed

--- Test/com.company/Test.java
+++ Test/com.company/Test.java
@@ -4,7 +4,11 @@ import com.company.operation.OperationFormat;
 
 public class Test {
     public static void main(String[] args) {
-        var operation = OperationFormat.StringToOperation("-8 + 45 - 9 * 7 - 6 / -5 * 9 * 8 + 9 / -4");
-        System.out.format("%.2f", operation.calculate());
+        var operation1 = OperationFormat.StringToOperation
+                ("(-8)+(45-9)*((7-(6/-5)*9*8)+9/-4)");
+        System.out.format("%.2f\n", operation1.calculate());
+        System.out.println();
+        var operation2 = OperationFormat.StringToOperation("-80");
+        System.out.format("%.2f\n", operation2.calculate());
     }
 }

87267eaf0f28545f058a825835df6d706592a4d8
taimoor <mtaimoor0167@gmail.com>
2022-09-02 15:45:19 +0500

    fix main scanner issue

--- src/com/company/operation/Main.java
+++ src/com/company/operation/Main.java
@@ -1,15 +1,12 @@
 package com.company.operation;
 
-import com.company.operation.operator.Operator;
-import com.company.operation.operator.OperatorFactory;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Operation operation = OperationFormat.StringToOperation(scanner.next());
+        System.out.print("Enter Expression: ");
+        Operation operation = OperationFormat.StringToOperation(scanner.nextLine());
         System.out.println(operation.calculate());
-        Operator add = OperatorFactory.getAddition();
     }
 }
